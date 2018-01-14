5098bf11129fd5b75b6b9874c3752b995f9d3088
Donev Yurii <yurii.donev@gmail.com>
2018-01-14 22:02:28 +0200

    build 14.01.2017

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export const Footer = () => {
-	return (
-		<div className='footer'>
-			<div className='footer-text'>{'Carp Feeder by DYV'} {'\u00A9'}</div>
-		</div>
-	);
-};
